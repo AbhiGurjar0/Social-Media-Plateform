a50e3169d61cd05015d1c1a78760a6cd1b503665
Abhi Gurjar <abhishekbainsla190@gmail.com>
2025-05-08 20:18:12 +0530

    tailwind setup done

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 router.get("/signin", (req, res) => {
-   
+   res.render("login");
 })
 router.get("/", (req, res) => { 
     res.send("login hai");
