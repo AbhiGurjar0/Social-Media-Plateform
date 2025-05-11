b45c0bd4c73642c2a3e425878d4e2f059503174a
Abhi Gurjar <abhishekbainsla190@gmail.com>
2025-05-11 22:12:40 +0530

    editprofile setup ✅

--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const userModel = require("../models/user-model");
-router.get("/signin", (req, res) => {
-    res.render("login");
-})
-router.get("/createPost", (req, res) => {
-    res.render("createPost");
-})
 
+//home page
 router.get("/", async (req, res) => {
     let user = await userModel.findOne({ email: "bishwalpw123456789@gmail.com" });
     res.render("home", { user });
 })
+
+//login/signup page
+
+router.get("/signin", (req, res) => {
+    res.render("login");
+})
+
+// post creation page 
+router.get("/createPost", (req, res) => {
+    res.render("createPost");
+})
+
+//edit profile page
+
+
+router.get("/profile/edit", (req, res) => {
+    res.render("editProfile");
+})
+
 
 module.exports = router;
