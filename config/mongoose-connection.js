8504db1dc4be35fb2c162023344313ad3b9e0ebb
Abhi Gurjar <abhishekbainsla190@gmail.com>
2025-07-17 15:35:13 +0530

    ✅✅

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
-// mongoose.connect(`mongodb://127.0.0.1:27017/social-media`)
-//     .then(function () {
-//         console.log("connected");
-//     })
-//     .catch(function (err) {
-//         console.log(err);
-//     })
+mongoose.connect(`mongodb://127.0.0.1:27017/social-media`)
+    .then(function () {
+        console.log("connected");
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
  
 
-mongoose.connect(process.env.MONGO_URI).
-    then(() => {
-        console.log("MongoDB Connected");
-    })
-    .catch(err => {
-        console.log("MongoDB connection err");
-        console.log(err.message);
+// mongoose.connect(process.env.MONGO_URI).
+//     then(() => {
+//         console.log("MongoDB Connected");
+//     })
+//     .catch(err => {
+//         console.log("MongoDB connection err");
+//         console.log(err.message);
 
-    }) 
+//     }) 
 module.exports = mongoose.connection;
